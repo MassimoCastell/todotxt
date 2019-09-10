8a6dfbec2d506b3f9f4f9e6f1d715db7456e7182
Castell, Massimo <Massimo.Castell@durr.com>
2019-09-10 09:34:53 +0200

    cmd bugfix

--- dos_command_settings.cmd
+++ dos_command_settings.cmd
@@ -1,4 +1,4 @@
 @echo off
 set ESC=
 REM doskey t=doskey s=if $1. equ . ("C:\Program Files (x86)\Git\bin\sh.exe" --login) else "C:\Program Files (x86)\Git\bin\sh.exe" --login -c "$*
-doskey t=if $1. equ . (todo) else todo $*
+doskey t=if $1. equ . (todo.py) else todo.py $*
