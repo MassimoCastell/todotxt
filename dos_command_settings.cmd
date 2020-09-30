fb41b1492bb1efdfe8b845299b7a093be0fc9af3
Castell, Massimo <Massimo.Castell@durr.com>
2020-09-30 14:46:35 +0200

    doskey corrected

--- dos_command_settings.cmd
+++ dos_command_settings.cmd
@@ -1,4 +1,3 @@
 @echo off
 set ESC=
-REM doskey t=doskey s=if $1. equ . ("C:\Program Files (x86)\Git\bin\sh.exe" --login) else "C:\Program Files (x86)\Git\bin\sh.exe" --login -c "$*
 doskey t=if $1. equ . (todo.py) else todo.py $*
